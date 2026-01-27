ba2fe06cab2e46e8d892c53e8cf5d36e49986f35
sadik254 <sadik254@gmail.com>
2026-01-27 18:59:06 +0600

    Booking model fix

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -9,5 +9,5 @@ class Booking extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['company_id', 'customer_id', 'vehicle_id', 'driver_id', 'service_type', ''hourly'', ''airport'', ''custom')', 'pickup_address', 'dropoff_address', 'pickup_time', 'passengers', 'distance_km', 'base_price', 'extras_price', 'total_price', 'status', ''confirmed'', ''assigned'', ''on_route'', ''completed'', ''cancelled')', 'notes'];
+    protected $fillable = ['company_id', 'customer_id', 'vehicle_id', 'driver_id', 'service_type', 'pickup_address', 'dropoff_address', 'pickup_time', 'passengers', 'distance_km', 'base_price', 'extras_price', 'total_price', 'status', 'notes'];
 }
