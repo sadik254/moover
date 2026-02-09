edae46208903902d1f869dc0bda934876ade5427
sadik254 <sadik254@gmail.com>
2026-02-09 18:39:56 +0600

    Nothing

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return response()->json(['message' => 'Welcome to the Taxi Booking API']);
 });
